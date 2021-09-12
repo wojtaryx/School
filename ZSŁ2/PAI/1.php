58d076a3d51046f04f58a648d1e25d489f1de2af
wojtaryx <89992492+wojtaryx@users.noreply.github.com>
2021-09-12 20:19:54 +0200

    Update 1.php

--- ZSŁ2/PAI/1.php
+++ ZSŁ2/PAI/1.php
@@ -1,44 +1,55 @@
-<?php 
- echo "text<br>";
- echo 'text<br>';  
- $name="Krystyna";
- echo "Imię: $name<br>";
+<?php
+echo "Text<br><t>";
+$name="Krystyna";
+echo "Imię: $name<br>";
 
- $calkowita=10;
- $bin=0b1010;//10
- $oct=011;
- $hex=0x11;//17
- 
+//typy danych!!
 
- $x=10.5;
- echo gettype ($x); //double
- $prawda=true;
- $fałsz=false;
- echo $hex;
- echo $prawda; //1
- echo $fałsz; //nic nie wyświetli
-  //składnia heredoc
- echo <<< P
-  
-    <hr>Imię: $name<br>
-    ZSł
-    <hr>
-    P;
- 
- 
-  
-    //2 wersja heredoca
+
+//całkowite
+$całkowita=10;
+$bin=0b1010; //10
+$oct=011; //9
+$hex=0x11; //17
+
+
+//zmiennoprzecinkowe
+$x=10.5;
+echo gettype($x);
+
+
+//logiczna
+$prawda=true;
+$fałsz=false;
+
+
+//składnia heredoc
+//służy do wyświetlenia danych
+echo <<< E
+<hr> <br>Imię: $name<br><br>
+ZSŁ
+<hr>
+E;
+
+
+//2 wersja heredoca (nie działa mi)
+$text = <<< P
+<hr> <br>Imię: $name<br><br>
+ZSŁ
+<hr>
+P;
+
 
 //nowdoc
 echo <<< 'E'
 <hr>Imię: $name<br>
 ZSŁ
-<hr>
+<br><br><hr>
 E;
 
-    //konkatenacja
-    echo "text1"."text2";
 
-    echo "Imię: \$name";
-    echo "Imię: $name";
+//konkatenacja
+echo "text1"."text2";
+//dzięki backslashowi traktuje zmienną jako stringa
+echo "<br><br>Imię: \$name"; 
 ?>
