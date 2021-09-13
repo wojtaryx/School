90c609c01294d05d4ea2d203fa8e32c7780be049
wojtaryx <89992492+wojtaryx@users.noreply.github.com>
2021-09-13 07:54:28 +0200

    Update operacja na zmiennych.php

--- ZSŁ2/PAI/operacja na zmiennych.php
+++ ZSŁ2/PAI/operacja na zmiennych.php
@@ -15,4 +15,33 @@
  }else{
      echo "<br>różne";
  }
+/*
+postinkrementacja $x++
+preinktrementacja ++$x
+postdekrementacja $x--
+predekrementacja --$x
+*/
+
+
+$x=1
+echo '<hr>' ,%x; //1
+echo ++$x; //2
+echo $x++; //2
+echo ++$x; ;<hr>; //4
+
+
+$x=1;
+echo $x; //1
+$x=$x++;
+echo $x; //1
+$x=++$x;
+echo $x; //2
+$y=++$x;
+echo $x; //3
+echo $y; //3
+$y=x++
+echo $x;  //4
+echo "$y<br>" //3
+
+
  ?>
